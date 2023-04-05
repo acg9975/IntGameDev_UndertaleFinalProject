fd3535b8fca9d9948543cf2685fef23a1ce33c82
Aaron Gill <acg975@nyu.edu>
2023-04-05 13:34:39 -0400

    Added player overworld movement script Added objects to sampleScene Necessary folders

---
+++ Assets/Scripts/OverworldMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverworldMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+    }
+
+
+
+}
