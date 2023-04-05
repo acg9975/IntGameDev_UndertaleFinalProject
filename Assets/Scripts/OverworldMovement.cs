def77b4f9d2ce8bbd1d2392caedfb7f9b648f6c7
BenjaminHecker <benhecker7@gmail.com>
2023-04-05 13:44:28 -0400

    Removed collision jitter

--- Assets/Scripts/OverworldMovement.cs
+++ Assets/Scripts/OverworldMovement.cs
@@ -7,27 +7,33 @@ public class OverworldMovement : MonoBehaviour
     [SerializeField]
     float speed = 5;
 
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.up * speed * Time.deltaTime);
+            rb.MovePosition(transform.position + Vector3.up * speed * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            rb.MovePosition(transform.position + Vector3.right * speed * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.down * speed * Time.deltaTime);
+            rb.MovePosition(transform.position + Vector3.down * speed * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            rb.MovePosition(transform.position + Vector3.left * speed * Time.deltaTime);
         }
-
     }
 
 
