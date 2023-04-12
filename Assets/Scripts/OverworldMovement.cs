c20d433648eb8cd389cf108bc511ecd3984f8c7d
Aaron Gill <acg975@nyu.edu>
2023-04-12 11:45:46 -0400

    attempted to change canMove - does not work

--- Assets/Scripts/OverworldMovement.cs
+++ Assets/Scripts/OverworldMovement.cs
@@ -10,6 +10,8 @@ public class OverworldMovement : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    public static bool canMove = true;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -18,22 +20,33 @@ public class OverworldMovement : MonoBehaviour
     private void Update()
     {
         // the direction the player should move is updated each frame
-        move = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (canMove)
+        {
+            move = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        if (move.x > 0)
-            move.x = 1;
-        if (move.x < 0)
-            move.x = -1;
+    
+            if (move.x > 0)
+                move.x = 1;
+            if (move.x < 0)
+                move.x = -1;
 
-        if (move.y > 0)
-            move.y = 1;
-        if (move.y < 0)
-            move.y = -1;
+            if (move.y > 0)
+                move.y = 1;
+            if (move.y < 0)
+                move.y = -1;
+        }
+        
     }
 
     private void FixedUpdate()
     {
         // the player moves on FixedUpdate() for consistent movement each frame
-        rb.MovePosition(transform.position + move * speed * Time.fixedDeltaTime);
+        if (canMove)
+        {
+            rb.MovePosition(transform.position + move * speed * Time.fixedDeltaTime);
+
+        }
     }
+
+
 }
