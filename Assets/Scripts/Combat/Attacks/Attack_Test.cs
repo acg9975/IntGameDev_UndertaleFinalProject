9fff4b4ffc63a6769d6cf31286632e41c5923535
BenjaminHecker <benhecker7@gmail.com>
2023-04-12 12:53:05 -0400

    Added CombatButton script Setup Attack script structure Sorted combat scripts into folders Renamed NPCDialogueEditor script

---
+++ Assets/Scripts/Combat/Attacks/Attack_Test.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Test", menuName = "Combat/Attacks/Test")]
+public class Attack_Test : Attack
+{
+    public override void Run()
+    {
+        base.Run();
+    }
+
+    public override void Stop()
+    {
+        base.Stop();
+    }
+}
--- Assets/Scripts/Combat/EnemyBehavior.cs
+++ Assets/Scripts/Combat/EnemyBehavior.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CombatBehavior : MonoBehaviour
+public class EnemyBehavior : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
