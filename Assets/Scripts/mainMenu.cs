6247b7ce4cb8b3d848f5d666f3ba81da144bd5c4
Aaron Gill <acg975@nyu.edu>
2023-05-08 13:07:03 -0400

    Player Prefs cleared out on main menu adjusted hitboxes in storage and sprite ordering

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -10,12 +10,14 @@ public class mainMenu : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && SceneManager.GetActiveScene().name == "MainMenu")
         {
+            PlayerPrefs.DeleteAll();
             SceneManager.LoadScene("front (spawn)");
             //Change to whatever the first level is in the modern version
         }
         else if (Input.GetKeyDown(KeyCode.Space) && SceneManager.GetActiveScene().name == "DeathScene") 
         {
             //load the last scene the player was at
+            PlayerPrefs.DeleteAll();
             Vector3 pos = new Vector3(PlayerPrefs.GetFloat("lastOverworldX"), PlayerPrefs.GetFloat("lastOverworldY"), 0);
             SceneTransition.ChangeScene(PlayerPrefs.GetString("lastScene"), pos);
         }
