cee84ba4ae1151c50b99f1960870405f137b1815
BenjaminHecker <benhecker7@gmail.com>
2023-05-08 01:31:13 -0400

    - Fixed SceneTransition fade

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -7,7 +7,8 @@ public class SceneTransition : MonoBehaviour
 {
     public static SceneTransition instance;
 
-    [SerializeField] private GameObject FOS;
+    //[SerializeField] private GameObject FOS;
+    [SerializeField] private Animator fadeAnim;
     private static Vector3 playerPos;
     public static string previousScene;
 
@@ -53,8 +54,11 @@ public class SceneTransition : MonoBehaviour
         //make the scene fadeout
         //- create an object that becomes completely black over two seconds and covers the screen
         //- change scene after two seconds 
-        GameObject fos = Instantiate(FOS, playerPos, Quaternion.identity);//This used to be referencing the overworld script 
-        fos.GetComponent<FadeOutSquare>().StartFadeIn();
+        //GameObject fos = Instantiate(FOS, playerPos, Quaternion.identity);//This used to be referencing the overworld script 
+        //fos.GetComponent<FadeOutSquare>().StartFadeIn();
+
+        fadeAnim.SetTrigger("FadeIn");
+
         StartCoroutine(LoadSceneRoutine(scene));
     }
 
@@ -64,24 +68,30 @@ public class SceneTransition : MonoBehaviour
         //- create an object that becomes completely black over two seconds and covers the screen
         //- change scene after two seconds 
         Debug.Log(position + " : Done");
-        GameObject fos = Instantiate(FOS, position, Quaternion.identity);
-        fos.GetComponent<FadeOutSquare>().StartFadeIn();
+        //GameObject fos = Instantiate(FOS, position, Quaternion.identity);
+        //fos.GetComponent<FadeOutSquare>().StartFadeIn();
+
+        fadeAnim.SetTrigger("FadeIn");
+
         StartCoroutine(LoadSceneRoutine(scene, position));
     }
 
     private IEnumerator LoadSceneRoutine(string scene)
     {
         Debug.Log("Changing scene now");
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(scene);
     }
     private IEnumerator LoadSceneRoutine(string scene, Vector3 position)
     {
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene(scene);
-        yield return new WaitForSeconds(0.05f);
-        GameObject fos = Instantiate(FOS, position, Quaternion.identity);
-        fos.GetComponent<FadeOutSquare>().StartFadeOut();
+        //yield return new WaitForSeconds(0.05f);
+        //GameObject fos = Instantiate(FOS, position, Quaternion.identity);
+        //fos.GetComponent<FadeOutSquare>().StartFadeOut();
+
+        fadeAnim.SetTrigger("FadeOut");
+
         GameObject.Find("Player").GetComponent<Transform>().position = position;
     }
     public void onDeath()
